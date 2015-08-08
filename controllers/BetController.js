eddac7ce4e6779a7108715f780963b82e1f2d23c
Lars Hisken <l.hisken.92@gmail.com>
2015-08-08 15:03:07 +0200

    Removing /test from the BetController

--- controllers/BetController.js
+++ controllers/BetController.js
@@ -128,32 +128,6 @@ var BetController = function (botService) {
 
 	});
 
-	/**
-	 * Testing ReplyKeyboardMarkup
-	 */
-	this.botService.use('/test', function (from) {
-
-		var keyboardLayout = [
-			["A", "B"],
-			["C", "D"]
-		]
-		var replyKeyboardMarkup = {"keyboard": keyboardLayout, "one_time_keyboard": true, "selective": true};
-
-		self.botService.bookieBot.sendMessage({
-			"chat_id": from.chat.id,
-			"text": "I am testing the keyboard!",
-			"reply_to_message_id": from.message_id,
-			"reply_markup": replyKeyboardMarkup
-		})
-		.then(function (to) {
-			console.log('I sent a reply to:', to.reply_to_message.message_id);
-		})
-		.fail(function (error) {
-
-		});
-
-	});
-
 	return this.botService;
 };
 
